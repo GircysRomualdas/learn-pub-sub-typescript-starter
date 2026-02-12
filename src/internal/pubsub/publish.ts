016eec773261bbf5a05ca6975b0d23f465f09794
Romas <r2udis@gmail.com>
2026-02-12 22:26:10 +0200

    Add publish message pack

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export function publishJSON<T>(
   ch: ConfirmChannel,
@@ -14,6 +15,29 @@ export function publishJSON<T>(
       routingKey,
       content,
       { contentType: "application/json" },
+      (err) => {
+        if (err) reject(err);
+        else resolve();
+      },
+    );
+  });
+}
+
+export function publishMsgPack<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const encoded = encode(value);
+  const buffer = Buffer.from(encoded);
+
+  return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange,
+      routingKey,
+      buffer,
+      { contentType: "application/x-msgpack" },
       (err) => {
         if (err) reject(err);
         else resolve();
